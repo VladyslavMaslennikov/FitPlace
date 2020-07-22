8043290b37930ce41449c78ca00324474e320aee
VladyslavMaslennykov <vladyslav.maslennikov@quantag-it.com>
2020-07-22 17:22:03 +0300

    Auth screen

--- FitPlace/ViewControllers/Auth/AuthViewController.swift
+++ FitPlace/ViewControllers/Auth/AuthViewController.swift
@@ -9,14 +9,40 @@
 import UIKit
 
 class AuthViewController: UIViewController {
+    
+    @IBOutlet weak var separatorLine: UIView!
+    @IBOutlet weak var informTextView: UITextView!
+    @IBOutlet weak var shadowView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+//Авторизуясь, вы соглашаетесь с политикой конфиденциальности, договором оферты и условиями использования
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupUI()
     }
     
 
+    private func setupUI() {
+        let font = UIFont(name: "Futura", size: 13)
+        let alignment: NSTextAlignment = .center
+        let plainTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.font: font as Any
+        ]
+        let underlineTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.underlineStyle: 1,
+            NSAttributedString.Key.underlineColor: UIColor.black,
+            NSAttributedString.Key.font: font as Any
+        ]
+        let plainText = NSAttributedString(string: "Авторизуясь, вы соглашаетесь с ", attributes: plainTextAttributes)
+        let underlinedText = NSAttributedString(string: "политикой конфиденциальности, договором оферты и условиями использования", attributes: underlineTextAttributes)
+        let compoundString = NSMutableAttributedString(attributedString: plainText)
+        compoundString.append(underlinedText)
+        informTextView.attributedText = compoundString
+        informTextView.textAlignment = alignment
+    }
    
 
 }
