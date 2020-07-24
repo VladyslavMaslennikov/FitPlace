7f426cd031c223e109a533ea94995a4a9ac1b5fd
VladyslavMaslennykov <vladyslav.maslennikov@quantag-it.com>
2020-07-24 22:29:57 +0300

    radius fixes

---
+++ FitPlace/ViewControllers/Pay/PayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PayViewController.swift
+//  FitPlace
+//
+//  Created by Vladyslav on 24.07.2020.
+//  Copyright © 2020 com.quantag-it. All rights reserved.
+//
+
+import UIKit
+
+class PayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
